e394881ff062d189559e6921ed83da389a79d0c8
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-25 13:39:19 -0500

    Typescript - Type Guards (For runtime Input params)

---
+++ type-script/type-guards.ts
@@ -0,0 +1,32 @@
+interface PersonTG {
+  firstName: string;
+  lastName: string;
+  isVisitor?: boolean;
+}
+
+function generateEmailTG(person: PersonTG): string | undefined {
+  if (person.isVisitor) {
+    return undefined;
+  } else {
+    return `${person.firstName}${person.lastName}@gmail.com`;
+  }
+}
+
+function isPerson(potentialPerson: any): boolean {
+  if ("firstName" in potentialPerson && "lastName" in potentialPerson) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function printEmailIfPerson(potentialPerson: any): void {
+  if (isPerson(potentialPerson)) {
+    console.log(generateEmailTG(potentialPerson));
+  } else {
+    console.log("input is not a Person");
+  }
+}
+
+printEmailIfPerson({ firstName: "John", lastName: "Doe" }); // JohnDoe@gmail.com
+printEmailIfPerson({ firstName: "John", lastName_: "Doe" }); // input is not a Person
