4fb2dd36b03070b8b6cf2535099e2cd20cb40a4e
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-25 12:51:09 -0500

    Typescript - Basic Types

---
+++ type-script/basic-types.ts
@@ -0,0 +1,12 @@
+const name = "BHOPAL";
+console.log(`Hello ${name}`);
+
+// Regular Way
+const a = "Hello";
+const b = 5;
+const c = false;
+
+// Typescript Way
+const aa: string = "Hello";
+const bb: number = 5;
+const cc: boolean = false;
