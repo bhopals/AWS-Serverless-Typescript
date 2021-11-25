98acd1da9503b6c5f210729938b9760aec583550
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-25 12:51:32 -0500

    Typescript - Basic Types

--- type-script/basic-types.ts
+++ type-script/basic-types.ts
@@ -1,12 +1,21 @@
-const name = "BHOPAL";
-console.log(`Hello ${name}`);
+const userName = "BHOPAL";
+console.log(`Hello ${userName}`);
 
 // Regular Way
-const a = "Hello";
-const b = 5;
-const c = false;
+const string1 = "Hello";
+const number1 = 5;
+const boolean1 = false;
+const array1 = [];
+const object1 = {};
 
 // Typescript Way
-const aa: string = "Hello";
-const bb: number = 5;
-const cc: boolean = false;
+const tstring1: string = "Hello";
+const tnumber1: number = 5;
+const tboolean1: boolean = false;
+const tarray1: string[] = []; // only String Types allowed - Preferred
+tarray1.push(string1);
+tarray1.push(number1 as any); // Type Casting - tarray1.push(number1) will give an error as only string types are allowed
+
+const tarray2: any[] = []; // Any Types allowed - Less Preferred
+
+const tobject1: object = {};
