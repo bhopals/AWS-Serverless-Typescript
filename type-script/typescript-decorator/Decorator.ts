f3b35fe7556617d7216aaaaf4565b692ec9c9d1d
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-25 19:04:27 -0500

    Typescript - object property Decorators

--- type-script/typescript-decorator/Decorator.ts
+++ type-script/typescript-decorator/Decorator.ts
@@ -1,12 +1,9 @@
-class Manager {
-  @watchChange
-  someProperty: string;
-}
+// An Object
+const someObject = {
+  someProperty: "initial",
+};
 
-const manager: Manager = new Manager();
-manager.someProperty = "123";
-manager.someProperty = "456";
-
+// Decorator - watchChange
 function watchChange(target: any, key: string) {
   let property = target[key];
 
@@ -23,3 +20,39 @@ function watchChange(target: any, key: string) {
     enumerable: true,
   });
 }
+
+// Decorator Factory - linkValue
+function linkValue(otherObject: any) {
+  return function (target: any, key: string) {
+    let property = target[key];
+
+    const getter = () => property;
+    const setter = (newVal: any) => {
+      console.log(`${key as string} changed from ${property} to ${newVal}`);
+      property = newVal;
+      otherObject[key] = newVal;
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      configurable: true,
+      enumerable: true,
+    });
+  };
+}
+class Manager {
+  //METHOD 1
+  // @watchChange
+  @linkValue(someObject)
+  someProperty: string;
+}
+
+//METHOD 2
+// watchChange(Manager.prototype, "someProperty");
+
+const manager: Manager = new Manager();
+manager.someProperty = "123";
+console.log("someObject.someProperty:" + someObject.someProperty);
+manager.someProperty = "456";
+console.log("someObject.someProperty:" + someObject.someProperty);
