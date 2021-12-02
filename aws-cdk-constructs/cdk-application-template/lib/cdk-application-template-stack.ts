d00ee2986758aff986ac8d19591d98c01b348d19
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-02 08:59:10 -0500

    AWS CDK App - CDK `CfnParameter`

--- aws-cdk-constructs/cdk-application-template/lib/cdk-application-template-stack.ts
+++ aws-cdk-constructs/cdk-application-template/lib/cdk-application-template-stack.ts
@@ -1,12 +1,19 @@
 import * as cdk from "@aws-cdk/core";
 import { Bucket } from "@aws-cdk/aws-s3";
-import { CfnOutput, Duration } from "@aws-cdk/core";
+import { CfnOutput, Duration, CfnParameter } from "@aws-cdk/core";
 export class CdkApplicationTemplateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const duration = new CfnParameter(this, "duration", {
+      type: "Number",
+      minValue: 1,
+      maxValue: 10,
+      default: 6,
+    });
+
     const myBucket = new Bucket(this, "someBucket", {
-      lifecycleRules: [{ expiration: Duration.days(10) }],
+      lifecycleRules: [{ expiration: Duration.days(duration.valueAsNumber) }],
     });
 
     new CfnOutput(this, "myBucket", {
