ef7fe32815a9b82d8139e8cb58d23fa4fb450813
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-02 08:50:35 -0500

    AWS CDK App - CDK `CfnOutput`

--- aws-cdk-constructs/cdk-application-template/lib/cdk-application-template-stack.ts
+++ aws-cdk-constructs/cdk-application-template/lib/cdk-application-template-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from "@aws-cdk/core";
 import { Bucket } from "@aws-cdk/aws-s3";
-import { Duration } from "@aws-cdk/core";
+import { CfnOutput, Duration } from "@aws-cdk/core";
 export class CdkApplicationTemplateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, "someBucket", {
+    const myBucket = new Bucket(this, "someBucket", {
       lifecycleRules: [{ expiration: Duration.days(10) }],
+    });
+
+    new CfnOutput(this, "myBucket", {
+      value: myBucket.bucketName,
     });
   }
 }
